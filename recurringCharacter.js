d6bdf5580ef25961a00fb3cd6ca626b2571d303b
Hemant Ghatawal <ghatawal35@gmail.com>
2026-01-13 13:14:53 +0530

    case of zero in the recurring character question

--- recurringCharacter.js
+++ recurringCharacter.js
@@ -8,19 +8,23 @@
 // Givent an array = [2,5,1,3]
 // It should return undefined
 
+// Givent an array = [2,0,0,3]
+// It should return 0
+
 function recurringCharacter(array) {
   const obj = {};
   for (let i = 0; i < array.length; i++) {
-    if (obj[array[i]]) {
+    if (obj[array[i]] !== undefined) {
+      // (2) or this undefined condition
       return array[i];
     } else {
-      obj[array[i]] = array[i];
+      obj[array[i]] = array[i]; // (1) either here true or 👆
     }
   }
 
   return undefined;
 }
 
-const data = [2, 5, 1, 3];
+const data = [2, 0, 0, 3];
 
 console.log(recurringCharacter(data));
