5c2ea174e5d5d9a6ed375ef9a3c8bb5d61a01232
Hemant Ghatawal <ghatawal35@gmail.com>
2026-02-14 16:46:18 +0530

    test sample fixed

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
 const arr = [1, 2, 3, 4, 5, 3, 2, 3, 1, 12, 3, 1, 0, 1];
-const obj = {0: 0, 1: 0}
+const obj = {}
 
-let count = 0;
 for (let i = 0; i < arr.length; i++) {
-  if(obj.has(
-
-
-
-    
-  ))
+  if(typeof obj[arr[i]] == 'undefined'){
+    obj[arr[i]] = 1
+  } else{
+    obj[arr[i]]++ 
+  }
 }
 
-console.log(count);
+console.log(obj);
