3a1d587b26ee36a75a5533eb59f4babd3ed7ed39
Hemant Ghatawal <ghatawal35@gmail.com>
2026-02-20 18:23:20 +0530

    bubbleSort2

--- Algo/BubbleSort.js
+++ Algo/BubbleSort.js
@@ -23,7 +23,21 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log(bubbleSort(arr));
+function bubbleSort2(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort2(arr));
 
 // another sokution
 // function bubbleSort(array) {
