b5499a2ea09e3274e918347af7a6250cd2528ba7
Hemant Saini <hemant@maisonnoiii.com>
2026-09-13 12:51:51 +0530

    feat(two-pointers): add 2 DSA files and fix remove duplicates II
    Add stub 3sum solution file
    Add full working solution for LeetCode 977 Squares of a Sorted Array
    Fix removeDuplicates II implementation, remove debug logs and clean up code

--- dsa-patterns/array/two-pointers/remove-duplicates-from-sorted-array-II.js
+++ dsa-patterns/array/two-pointers/remove-duplicates-from-sorted-array-II.js
@@ -5,19 +5,17 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let write = 2;
-  for (let read = 2; read < nums.length; read++) {
-    console.log("read ", read)
-    console.log("write ", write)
-    console.log("write - 2 ", write - 2)
-    console.log("nums ", nums)
-    if (nums[read] !== nums[write - 2]) {
-      nums[write] = nums[read];
-      write++;
+    let write = 3;
+    for(let read = 2; read < nums.length; read++){
+      if(nums[read] !== nums[write - 3]){
+        nums[write] = nums[read];
+        write++
+      }
     }
-  }
-  console.log(nums)
-  return write;
+
+    console.log(nums)
+    return write
 };
-                           // w.          r
+
+
 console.log(removeDuplicates([0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 3]));
