d8b5b6ca7f666259c044439db523ba055d022fbc
Hemant Saini <hemant@maisonnoiii.com>
2026-09-23 07:08:02 +0530

    largestPerimeter.js

---
+++ dsa-patterns/array/sorting/largestPerimeter.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  for (let i = nums.length - 3; i >= 0; i--) {
+    let a = nums[i];
+    let b = nums[i + 1];
+    let c = nums[i + 2];
+
+    if (a + b > c) {
+      return a + b + c;
+    }
+  }
+  return 0
+};
+
+console.log(largestPerimeter([1,2,1,10]));
